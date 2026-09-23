f2beadf96f04428b1a8bcacdc9bccdfb1d995ade
AMAN LUTHRA <amanluthra804@gmail.com>
2026-09-23 19:14:58 +0530

    PostgreSQl:- Ques1 Alter Table

--- AlterTable.sql
+++ AlterTable.sql
@@ -1,10 +1,13 @@
-CREATE TABLE testing_table(
-  name varchar(255),
-  contact_name varchar(255),
-  roll_no varchar(255)
+CREATE TABLE testing_table (
+    name VARCHAR(255),
+    contact_name VARCHAR(255),
+    roll_no VARCHAR(255)
 );
-ALTER TABLE testing_table DROP COLUMN name;
-ALTER TABLE testing_table RENAME COLUMN contact_name To username;
-ALTER TABLE testing_table ADD COLUMN first_name varchar(255),
-						  ADD COLUMN last_name varchar(255);
-ALTER TABLE testing_table ALTER COLUMN roll_no TYPE int;
+
+ALTER TABLE testing_table RENAME COLUMN contact_name TO username;
+
+ALTER TABLE testing_table 
+    DROP COLUMN name,
+    ADD COLUMN first_name VARCHAR(255),
+    ADD COLUMN last_name VARCHAR(255),
+    ALTER COLUMN roll_no TYPE INT USING roll_no::INT;
