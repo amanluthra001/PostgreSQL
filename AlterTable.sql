bed4eceb4015edb39f4af3faa384084b4f74e31c
AMAN LUTHRA <amanluthra804@gmail.com>
2026-09-23 18:58:48 +0530

    PostgreSQl:- Ques1 Alter Table

--- AlterTable.sql
+++ AlterTable.sql
@@ -4,7 +4,7 @@ CREATE TABLE testing_table(
   roll_no varchar(255)
 );
 ALTER TABLE testing_table DROP COLUMN name;
-ALTER TABLE testing_table RENAME COLUMN contact_name to username;
+ALTER TABLE testing_table RENAME COLUMN contact_name To username;
 ALTER TABLE testing_table ADD COLUMN first_name varchar(255),
 						  ADD COLUMN last_name varchar(255);
 ALTER TABLE testing_table ALTER COLUMN roll_no TYPE int;
